0cba84d28a456e8664ef9b331de15ef467461dca
en.rujikorn@gmail.com <en.rujikorn@gmail.com>
2024-08-25 11:26:15 +0000

    add sql statements

--- 3.transfer-ownership.sql
+++ 3.transfer-ownership.sql
@@ -1,2 +1,14 @@
 -- Databricks notebook source
+-- MAGIC %md
+-- MAGIC
+-- MAGIC ### Transfer ownership
+-- MAGIC
+-- MAGIC [docs](https://learn.microsoft.com/en-us/azure/databricks/data-governance/unity-catalog/manage-privileges/ownership#transfer-ownership)
 
+-- COMMAND ----------
+
+ALTER <securable-type> <securable-name> OWNER TO <principal>;
+
+-- COMMAND ----------
+
+ALTER TABLE mycatalog.myschema.orders OWNER TO `accounting`;
