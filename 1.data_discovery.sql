2138c20285e1e737e74c447a0fc35d130ce54af2
en.rujikorn@gmail.com <en.rujikorn@gmail.com>
2024-08-25 09:23:11 +0000

    initial commit

---
+++ 1.data_discovery.sql
@@ -0,0 +1,23 @@
+-- Databricks notebook source
+SELECT *
+FROM `system`.information_schema.tables
+WHERE table_name = 'results';
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC
+-- MAGIC ##### The difference between this information schema here within each of the catalogs and the one on the system
+-- MAGIC
+-- MAGIC - system information schema gives you all object within this metastore
+-- MAGIC - whereas the one within catalog only gives you the information within the catalog
+
+-- COMMAND ----------
+
+SELECT *
+FROM `demo_catalog`.information_schema.tables
+WHERE table_name = 'results';
+
+-- COMMAND ----------
+
+
